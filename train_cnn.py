59ebb639a150fd423b2b1c365b9c915942020626
Quang Hung Dang <hungdaqq@gmail.com>
2023-06-25 01:34:02 +0700

    Update

--- train_cnn.py
+++ train_cnn.py
@@ -1,45 +1,46 @@
 import tensorflow as tf
-from tensorflow.keras.applications import EfficientNetB0
+from tensorflow.keras.applications import EfficientNetB1
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.callbacks import ProgbarLogger
 
 # Set the input image size and number of classes
-input_shape = (128, 128, 3)
+input_shape = (64, 128, 3)
 num_classes = 2
 num_epochs = 10
 
 # Create the EfficientNet model
-base_model = EfficientNetB0(weights='imagenet', include_top=False, input_shape=input_shape)
+base_model = EfficientNetB1(weights='imagenet', include_top=False, input_shape=input_shape)
 model = tf.keras.Sequential([
     base_model,
     tf.keras.layers.GlobalAveragePooling2D(),
     tf.keras.layers.Dense(num_classes, activation='sigmoid')
 ])
 
+model.summary()
 # Compile the model
 model.compile(optimizer='adam',
               loss='categorical_crossentropy',
               metrics=['accuracy'])
 
 # Prepare your data using an ImageDataGenerator
-train_datagen = ImageDataGenerator(rescale=1./255,
+train_datagen = ImageDataGenerator(rescale=None,
                                    rotation_range=20,
                                    width_shift_range=0.1,
                                    height_shift_range=0.1,
                                    shear_range=0.2,
                                    zoom_range=0.2,
-                                   horizontal_flip=True)
+                                   vertical_flip=True)
 
-val_datagen = ImageDataGenerator(rescale=1./255)
+val_datagen = ImageDataGenerator(rescale=None)
 
 train_generator = train_datagen.flow_from_directory(
-    '/home/hung/DL2023/DriverDrowsinessDetection/classify/',
+    '/home/hung/DL2023/DriverDrowsinessDetection/classify_data/train/',
     target_size=(input_shape[0], input_shape[1]),
     batch_size=32,
     class_mode='categorical')
 
 val_generator = val_datagen.flow_from_directory(
-    '/home/hung/DL2023/DriverDrowsinessDetection/val/',
+    '/home/hung/DL2023/DriverDrowsinessDetection/classify_data/val/',
     target_size=(input_shape[0], input_shape[1]),
     batch_size=32,
     class_mode='categorical')
@@ -55,5 +56,5 @@ model.fit(train_generator,
           callbacks=[progbar_logger])
 
 # Save the model as a .pb file
-pb_path = './model.pb'
+pb_path = './cnn_model/EfficientNetB1/'
 tf.saved_model.save(model, pb_path)
